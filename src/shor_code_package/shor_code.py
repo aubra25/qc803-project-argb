ee954eff173b6a3d3c893587b86770ae488374ce
argb <aubra25@student.sdu.dk>
2025-12-05 16:58:06 +0100

    [3] Added testing to see that each X and Z pauli error gives expected syndrome measurement.

--- src/shor_code_package/shor_code.py
+++ src/shor_code_package/shor_code.py
@@ -1,12 +1,31 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, AncillaRegister, QuantumRegister, ClassicalRegister
 import numpy as np
+from qiskit_aer import AerSimulator
 
-class ShorCode():
+class ShorQubit():
     def __init__(self):
         """
         Initialize the ShorCode instance.
         """
-        self._circuit = QuantumCircuit(9)
+        self.num_qubits = 9 + 8
+
+    def circuit(self):
+        """
+        Returns quantum circuit for initializing a state in the Shor code, measuring stabilisers, and correcting
+        the syndromes.
+        """
+        #Initialize circuit
+        n_qubits = 9
+        n_ancillas = 8
+        qc = QuantumCircuit(n_qubits + n_ancillas)
+        
+        #Compse components
+        qc.compose(self.encoder(), inplace = True)
+        qc.barrier()
+        qc.compose(self.stabilizer_measurement_circuit(), inplace = True)
+
+        return qc
+
 
     def encoder(self):
         """
@@ -29,6 +48,36 @@ class ShorCode():
     
         return qc
     
+    def stabilizer_measurement_circuit(self):
+        """
+        Return a QuantumCircuit measuring the stabilizers of the Shor code.
+        
+        Each stabilizer has eigenvalues +1 or -1, so Quantum Phase Estimation
+        can be utilized with a single ancilla per stabilizer to get the
+        measurement outcome of the stabilizer non-destructively.
+        """
+        #Initialize circuit
+        stabilizers = self.get_stabilizers()
+        qc = QuantumCircuit(9)
+        ancilla_register = AncillaRegister(len(stabilizers))
+        classical_register = ClassicalRegister(len(stabilizers), name = 'Stabilizer measurements')
+        qc.add_register(ancilla_register)
+        qc.add_register(classical_register)
+
+        #Add phase estimation for each stabilizer
+        for ancilla, classic_bit, stabilizer in zip(ancilla_register, classical_register, stabilizers):
+            cu = stabilizer.to_gate().control(1)
+            qc.h(ancilla)
+            qc.append(cu, [ancilla, *range(9)])
+            qc.h(ancilla)
+            qc.measure(ancilla, classic_bit)
+
+            #Reset the ancilla for use in another round of error correction
+            qc.reset(ancilla)
+            qc.barrier()
+
+        return qc
+
     def get_stabilizers(self):
         """
         Return a list of QuantumCircuits that implement the stabilizers of the Shor code.
@@ -36,20 +85,80 @@ class ShorCode():
         stabilizer_circuits = []
         #The Shor Code has 8 stabilizers. First the stabilizers involving X are constructed.
         for n in [0, 3]:
-            qc = QuantumCircuit(9)
+            qc = QuantumCircuit(9, name=f"X_{n}X_{n+1}X_{n+2}X_{n+3}X_{n+4}X_{n+5}")
             for q in range(n, n + 6):
                 qc.x(q)
             stabilizer_circuits.append(qc)
         
         #The stabilizers involving Z.
         for n in [0, 1, 3, 4, 6, 7]:
-            qc = QuantumCircuit(9)
+            qc = QuantumCircuit(9, name = f"Z_{n}Z_{n+1}")
             qc.z(n)
             qc.z(n+1)
             stabilizer_circuits.append(qc)
         
         return stabilizer_circuits
         
-        
+class ShorCircuit:
+    """
+    This class handles construction of QuantumCircuits using the Shor nine qubit quantum error correction code
+    """
+
+    def __init__(self, shor_code = ShorQubit()):
+        """
+        Initialize the ShorCircuit. A ShorCode to use must be injected.
+        """
+        self.sc = shor_code
+        self.aer = AerSimulator()
+        self._initialize_circuit()
+
+    def _initialize_circuit(self):
+        """
+        Initializises the encoder circuit
+        """
+        self._circuit = QuantumCircuit(self.sc.num_qubits)
+        self._circuit.compose(self.sc.encoder())
+
+    def x(self, qubit):
+        """
+        Add Pauli X in between encoding and syndrome measurement.
+        """
+        self._circuit.x(qubit)
+
+    def y(self, qubit):
+        """
+        Add Pauli Y in between encoding and syndrome measurement.
+        """
+        self._circuit.y(qubit)
+
+    def z(self, qubit):
+        """
+        Add Pauli Z in between encoding and syndrome measurement.
+        """
+        self._circuit.z(qubit)
+
+    def get_circuit(self):
+        """
+        Returns the QuantumCircuit built using the Shor encoding.
+        """
+        qc = self._circuit.copy()
+        qc.compose(self.sc.stabilizer_measurement_circuit(), inplace=True)
+        return qc
+    
+    def simulate_noiseless(self, shots = 1e3):
+        """
+        Run a simulation of the circuit using the Aer simulator. Returns the result object of the simulation.
+        """
+        qc = self.get_circuit()
+        result = self.aer.run(qc.decompose(), shots = shots).result()
+        return result
+
+
+    
+
+    
+    
+
+
 
 
--- tests/test_shor_package.py
+++ tests/test_shor_package.py
@@ -1,10 +1,11 @@
-from shor_code_package.shor_code import ShorCode
+from shor_code_package.shor_code import ShorQubit
 import qiskit.quantum_info as qi
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, AncillaRegister
+from qiskit_aer import AerSimulator
 import numpy as np
 import pytest
 
-class TestShorCode():
+class TestShorQubit():
     ##########################################
     # Useful resources for use in tests
     ##########################################
@@ -21,12 +22,18 @@ class TestShorCode():
         ghz_minus = qi.Statevector([1/np.sqrt(2), 0, 0, 0, 0, 0, 0, -1/np.sqrt(2)])
         return ghz_minus ^ ghz_minus ^ ghz_minus
 
+    aer = AerSimulator()
+    def aer_simulation(self, qc, shots = 10):
+        result = self.aer.run(qc.decompose(), shots = shots).result()
+        return result.get_counts()
+
     ##########################################
     # Tests
     ##########################################
+    #Assignment 1
     def test_encode_0_returns_0_logical_state(self):
         #Arrange
-        encoder = ShorCode().encoder()
+        encoder = ShorQubit().encoder()
         target = self.logical_0()
         
         #Act
@@ -40,7 +47,7 @@ class TestShorCode():
         target = self.logical_1()
         
         #Ensure a |1> is the input to the encoder
-        qc = self.ket_1_circuit().compose(ShorCode().encoder())
+        qc = self.ket_1_circuit().compose(ShorQubit().encoder())
 
         #Act
         final_state = qi.Statevector.from_instruction(qc)
@@ -48,11 +55,12 @@ class TestShorCode():
         #Assert
         assert np.isclose(qi.state_fidelity(final_state, target), 1)
 
-    @pytest.mark.parametrize("stabilizer", ShorCode().get_stabilizers())
+    #Assignment 2
+    @pytest.mark.parametrize("stabilizer", ShorQubit().get_stabilizers())
     def test_logical_0_is_unaffected_by_stabilisers(self, stabilizer):
         #Arrange 
-        sc = ShorCode()
-        qc = sc.encoder().compose(stabilizer)
+        sq = ShorQubit()
+        qc = sq.encoder().compose(stabilizer)
         target = self.logical_0()
 
         #Act
@@ -61,11 +69,11 @@ class TestShorCode():
         #Assert
         assert np.isclose(qi.state_fidelity(final_state, target), 1)
 
-    @pytest.mark.parametrize("stabilizer", ShorCode().get_stabilizers())
+    @pytest.mark.parametrize("stabilizer", ShorQubit().get_stabilizers())
     def test_logical_1_is_unaffected_by_stabilisers(self, stabilizer):
         #Arrange 
-        sc = ShorCode()
-        qc = self.ket_1_circuit().compose(sc.encoder().compose(stabilizer))
+        sq = ShorQubit()
+        qc = self.ket_1_circuit().compose(sq.encoder().compose(stabilizer))
         target = self.logical_1()
 
         #Act
@@ -73,3 +81,61 @@ class TestShorCode():
 
         #Assert
         assert np.isclose(qi.state_fidelity(final_state, target), 1)
+
+    #Assignement 3
+    @pytest.mark.parametrize("qubit, syndrome", 
+                             [
+                                 (0,"00100000"),
+                                 (1,"00110000"),
+                                 (2,"00010000"),
+                                 (3,"00001000"),
+                                 (4,"00001100"),
+                                 (5,"00000100"),
+                                 (6,"00000010"),
+                                 (7,"00000011"),
+                                 (8,"00000001"),
+                              ])
+    def test_bit_flip_gives_unique_syndrome(self, qubit, syndrome):
+        #Arrange
+        sq = ShorQubit()
+        qc = sq.encoder()
+        qc.add_register(AncillaRegister(8))
+        qc.x(qubit)
+        qc.compose(sq.stabilizer_measurement_circuit(), inplace=True)
+
+        #Act
+        result = self.aer_simulation(qc)
+
+        #Assert
+        #There should only be a single syndrome as the simulation is noiseless
+        assert len(result.keys()) == 1
+        assert list(result.keys())[0] == syndrome[::-1] #Bit order is reversed in qiskit
+
+    @pytest.mark.parametrize("qubit, syndrome", 
+                            [
+                                (0,"10000000"),
+                                (1,"10000000"),
+                                (2,"10000000"),
+                                (3,"11000000"),
+                                (4,"11000000"),
+                                (5,"11000000"),
+                                (6,"01000000"),
+                                (7,"01000000"),
+                                (8,"01000000"),
+                            ])
+    def test_phase_flip_gives_degenerate_syndrome(self, qubit, syndrome):
+        #Arrange
+        sq = ShorQubit()
+        qc = sq.encoder()
+        qc.add_register(AncillaRegister(8))
+        qc.z(qubit)
+        qc.compose(sq.stabilizer_measurement_circuit(), inplace=True)
+
+        #Act
+        result = self.aer_simulation(qc)
+
+        #Assert
+        #There should only be a single syndrome as the simulation is noiseless
+        assert len(result.keys()) == 1
+        assert list(result.keys())[0] == syndrome[::-1] #Bit order is reversed in qiskit
+
